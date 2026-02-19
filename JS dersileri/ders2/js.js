27ffad72520199cf970ab3e3725f3872a9df8eb0
Cannibal <kamalkirsudov@gmail.com>
2026-02-19 14:48:25 +0300

    Add files via upload

---
+++ JS dersileri/ders2/js.js
@@ -0,0 +1,9 @@
+function resim1(){
+    document.getElementById("resim").src="https://www.pngall.com/wp-content/uploads/19/Poison-Apple-Wicked-Enchantment-Artifact-PNG.png";
+}
+function resim2(){
+    document.getElementById("resim").src="https://www.pngall.com/wp-content/uploads/19/Two-Juicy-Green-Apple-PNG.png";
+}
+function resim3(){
+   document.getElementById("resim").src="https://www.pngall.com/wp-content/uploads/19/Poison-Apple-Wicked-Enchantment-Artifact-PNG.png";
+}
